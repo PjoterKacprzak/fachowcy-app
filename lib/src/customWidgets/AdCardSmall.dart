af95c279166001446a57302820b438d4094c2b52
Krystian Bendinger <krystian.bendinger@gmail.com>
2020-10-28 23:59:01 +0100

    added and initiate use icon to delete ad - user profile

--- lib/src/customWidgets/AdCardSmall.dart
+++ lib/src/customWidgets/AdCardSmall.dart
@@ -26,38 +26,75 @@ class AdCardSmall extends StatelessWidget {
           width: 160,
           child: ClipRRect(
             borderRadius: BorderRadius.circular(16),
-            child: Container(
-              decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 4.0)),
-              child: Column(
-                children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Container(
-                      margin: const EdgeInsets.all(8),
-                      height: 60,
-                      width: 120,
-                      color: Colors.black,
-                      child: Center(
-                        child: Text(
-                          "Foto",
-                          style: const TextStyle(color: Colors.white),
+            child: GestureDetector(
+              onTap:(){
+                print("Ad pressed");
+              },
+              child: Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 4.0)),
+                child: Column(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Container(
+                        margin: const EdgeInsets.all(8),
+                        height: 60,
+                        width: 120,
+                        color: Colors.black,
+                        child: Center(
+                          child: Text(
+                            "Foto",
+                            style: const TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Text(
-                    title,
-                    style: const TextStyle(color: Colors.white, fontSize: 24),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    text,
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 10),
-                ],
+                    Text(
+                      title,
+                      style: const TextStyle(color: Colors.white, fontSize: 24),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      text,
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
+                     Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: IconButton(
+                              icon: new Icon(Icons.edit),
+                              color: Colors.grey,
+                              onPressed: () {
+                                print("Edited!");
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: IconButton(
+                              icon: new Icon(Icons.delete),
+                              color: Colors.white,
+                              onPressed: () {
+                                print("Deleted!");
+                              },
+                            ),
+                          ),
+
+
+                        ],
+                      ),
+                  ],
+                ),
               ),
             ),
           ),
